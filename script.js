8aff8936478f7b4b339853799e8c76d2c9851b27
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 14:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var isDate = function (input) {
 	if( input instanceof Date && !isNaN(input)) return true;
 	if(typeof input ==="string"){
 		const newDate = new Date(input);
-		if(!isNaN(newDate)&&newDate != "Invalid Date") return true
+		if(!isNaN(newDate)&& newDate instanceof Date) return true
 	}
 	return false;
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+//const input = prompt("Enter Date.");
 alert(isDate(input));
